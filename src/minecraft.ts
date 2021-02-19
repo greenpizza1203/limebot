fa6c70563f23ea5a0d7414ec48ee470089b00769
greenpizza1203 <konakarthik12@gmail.com>
2021-02-19 10:40:42 -0500

    changes

--- src/minecraft.ts
+++ src/minecraft.ts
@@ -1,19 +1,40 @@
 import {Message} from "discord.js";
 import axios from "axios";
+import Compute from "@google-cloud/compute";
 
 const client = axios.create({
     baseURL: 'https://google-minecraft-server.herokuapp.com/'
 });
 
+const compute = new Compute({projectId: "minecraft-305223", keyFilename: "minecraft.json"});
+const zone = compute.zone('us-east4-c');
+const vm = zone.vm('minecraft');
+
+export async function getVMStatus() {
+    const [{status}] = await vm.getMetadata();
+    return status
+}
+
+export async function startVM() {
+    const result = await vm.start();
+    console.log(result[0])
+}
+
+export async function stopVM() {
+    const result = await vm.stop();
+    console.log(result[0])
+}
+
 export async function minecraft(msg: Message) {
     const command = msg.content.split(' ');
     if (command[1] == "status") {
-        const result = await client.get("status")
-        msg.reply(result.data)
-
+        const result = await getVMStatus()
+        msg.reply(`The minecraft server is \`${result}\``)
+    } else if (command[1] == "start") {
+        startVM()
+    } else if (command[1] == "stop") {
+        stopVM()
     }
 }
 
 
-// @ts-ignore
-minecraft({content: "!minecraft status", reply: console.log})
