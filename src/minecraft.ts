1b64bb70894dcff9db24860c75bccadb883bc41b
Karthikeya Kona <konakarthik12@gmail.com>
2021-02-21 11:12:41 -0500

    changes

--- src/minecraft.ts
+++ src/minecraft.ts
@@ -1,6 +1,8 @@
-import {Message} from "discord.js";
+import {Message, MessageEmbed} from "discord.js";
 import axios from "axios";
 import Compute from "@google-cloud/compute";
+
+import util from "minecraft-server-util";
 
 const client = axios.create({
     baseURL: 'https://google-minecraft-server.herokuapp.com/'
@@ -9,32 +11,72 @@ const client = axios.create({
 const compute = new Compute({projectId: "minecraft-305223", keyFilename: "minecraft.json"});
 const zone = compute.zone('us-east4-c');
 const vm = zone.vm('minecraft');
+let cacheIP;
+const {MessageAttachment} = require('discord.js');
+
+
+export async function getIP() {
+    // noinspection JSPotentiallyInvalidTargetOfIndexedPropertyAccess
+    return cacheIP ??= (await vm.getMetadata())[0].networkInterfaces[0].accessConfigs[0].natIP
+}
 
 export async function getVMStatus() {
     const [{status}] = await vm.getMetadata();
+
     return status
 }
 
-export async function startVM() {
-    const result = await vm.start();
-    console.log(result[0])
+export async function getMinecraftStatus() {
+    let data;
+    try {
+        data = await util.status(await getIP())
+    } catch (e) {
+        console.error(e)
+    }
+    return {embed: getEmbed(data)}
+
+
 }
 
-export async function stopVM() {
-    const result = await vm.stop();
-    console.log(result[0])
+function getEmbed(data) {
+    console.log(data)
+    const embed = new MessageEmbed()
+    embed.setTitle("Minecraft Server Status")
+    embed.setColor("#00ff00")
+    embed.setTimestamp()
+    embed.setDescription("Server is currently offline")
+    if (data) {
+        embed.setDescription(`Server is currently online with ${data.onlinePlayers} players`)
+        embed.addField("description", data.description.descriptionText)
+        embed.addField("version", data.version)
+        embed.addField("players", data.samplePlayers?.map(player=>player.name)?.join('\n'))
+        embed.setFooter(data.host)
+    }
+    let actualBuf = data.favicon.split(',')[1];
+    const imageStream = Buffer.from(actualBuf, 'base64');
+    const attachment = new MessageAttachment(imageStream, "favicon.png");
+    embed.attachFiles(attachment)
+    embed.setThumbnail("attachment://favicon.png");
+    return embed
+}
+
+// console.log(embed)
+
+
+export async function getStatus() {
+    const status = await getVMStatus()
+    if (status.toUpperCase() !== "RUNNING") return `The minecraft VM is \`${status}\``
+    return await getMinecraftStatus()
 }
 
 export async function minecraft(msg: Message) {
     const command = msg.content.split(' ');
     if (command[1] == "status") {
-        const result = await getVMStatus()
-        msg.reply(`The minecraft server is \`${result}\``)
+        let content = await getStatus();
+        msg.reply(content)
     } else if (command[1] == "start") {
-        startVM()
+        await vm.start()
     } else if (command[1] == "stop") {
-        stopVM()
+        await vm.stop();
     }
 }
-
-
