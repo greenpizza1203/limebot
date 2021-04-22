11e760131704cf156129b462ce6bd10f3329128c
Karthikeya Kona <konakarthik12@gmail.com>
2021-04-22 09:27:28 -0400

    attach emote

--- src/emote.ts
+++ src/emote.ts
@@ -2,14 +2,13 @@ import {Message} from "discord.js";
 
 export async function emote(msg: Message) {
     let name = msg.content.split(' ')[1];
-    let url = msg.content.split(' ')[2];
+    console.log(msg.attachments)
+    let url = msg.content.split(' ')[2] || msg.attachments.first()?.url;
     if (!name) {
         msg.reply("No name given for emote");
     } else if (!url) {
-        msg.reply("No url provided")
+        msg.reply("No url or image provided")
     } else {
         msg.guild.emojis.create(url, name)
     }
-
-
 }
