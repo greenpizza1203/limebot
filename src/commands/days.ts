9d7891d6497790aa774a16fff2d3e76cfe9e0882
Karthikeya Kona <konakarthik12@gmail.com>
2021-05-04 22:18:18 -0400

    date 10am

--- src/commands/days.ts
+++ src/commands/days.ts
@@ -2,7 +2,7 @@ import moment from "moment";
 import {addCommand} from "../commands";
 
 export async function days() {
-    const a = moment('May 22nd 2021, 11:00 am', 'MMMM Do YYYY, h:mm a');
+    const a = moment('May 22nd 2021, 10:00 am', 'MMMM Do YYYY, h:mm a');
     return `graduation ${a.fromNow()}`;
 }
 
