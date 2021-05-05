b514d6bfae8188cecee3a3f5cf1a7ed51f957380
Karthikeya Kona <konakarthik12@gmail.com>
2021-05-04 22:31:53 -0400

    hours for fucking leon

--- src/commands/days.ts
+++ src/commands/days.ts
@@ -6,4 +6,11 @@ export async function days() {
     return `graduation ${a.fromNow()}`;
 }
 
+export async function hours() {
+    const a = moment('May 22nd 2021, 10:00 am', 'MMMM Do YYYY, h:mm a');
+    let number = a.diff(moment.now(), 'hours', false);
+    return `graduation in ${number} hours`;
+}
+
+addCommand(hours)
 addCommand(days)
