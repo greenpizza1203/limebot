70a52b93083648626600ed67cbe32f8630f46ef2
Karthikeya Kona <konakarthik12@gmail.com>
2021-08-16 15:53:46 -0400

    changes

--- src/commands.ts
+++ src/commands.ts
@@ -3,14 +3,14 @@ import path from "path";
 import {REST} from "@discordjs/rest";
 
 import {SlashCommandBuilder} from "@discordjs/builders";
-import {CommandInteraction} from "discord.js";
+import {CommandInteraction, Snowflake} from "discord.js";
 
 import {Routes} from "discord-api-types/v9";
 
 
 const rest = new REST({version: '9'}).setToken(process.env.token);
-export const CLIENT_ID = '715571596152143934';
-export const GUILD_ID = '292745734409682944';
+export const CLIENT_ID: Snowflake = '715571596152143934';
+export const GUILD_ID:Snowflake = '292745734409682944';
 const commands = [];
 const funcs = {};
 
@@ -20,6 +20,7 @@ export async function loadCommands() {
     const commandFiles = fs.readdirSync(path.join(__dirname, 'commands'));
     for (const file of commandFiles) require(`./commands/${file}`);
     await rest.put(
+        //@ts-ignore
         Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
         {body: commands},
     );
@@ -34,6 +35,6 @@ export function addCommand(command: SlashCommandBuilder, func: () => string) {
 export function executeCommand(interaction: CommandInteraction) {
     let func = funcs[interaction.commandName];
     let output = func(interaction);
-    if(output) interaction.reply(output);
+    if (output) interaction.reply(output);
     return func();
 }
